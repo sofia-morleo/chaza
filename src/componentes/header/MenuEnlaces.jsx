3b7a1de4ab8d765b613b66ecbcfa9768a8037c3a
sofia-morleo <sofymleo@gmail.com>
2023-08-03 11:57:14 -0300

    fin_clase

---
+++ src/componentes/header/MenuEnlaces.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './header.css'
+
+const MenuEnlaces = () => {
+  return (
+    <>
+        <ul>
+            <li><a href='#'>Categorias</a></li>
+            <li><a href='#'>Salones</a></li>
+            <li><a href='#'>Imagen Personal</a></li>
+            <li><a href='#'>Contacto</a></li>
+        </ul>
+    </>
+    
+  )
+}
+
+export default MenuEnlaces
---
+++ src/componentes/header/header.jsx
@@ -0,0 +1,16 @@
+// rafce comando para que haga esto
+import React from 'react'
+import MenuEnlaces from './MenuEnlaces'
+
+const Encabezado = () => {
+  return (
+    <>
+      <div>PORTAL DE EVENTOS</div>
+      <MenuEnlaces>
+      </MenuEnlaces>
+    </>
+  )
+}
+
+export default Encabezado
+
